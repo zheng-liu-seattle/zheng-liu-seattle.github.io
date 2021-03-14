cc738b044ba3b2ad8b3463ce8e53f299bbdcb2ee
Zheng Liu <zheng.liu@grab.com>
2021-03-14 14:06:51 -0700

    Update test.js

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,1 +1,6 @@
-alert("456");
+alert('here');
+var tag = document.createElement("div");
+tag.style="height:300px;background-color:red;width:100%;";
+var text = document.createTextNode("Tutorix is the best e-learning platform");
+tag.appendChild(text);
+document.body.appendChild(tag);
