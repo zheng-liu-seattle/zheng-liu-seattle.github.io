a2298ee53f6a13d3a97c61edc099cac7d47c0a8f
Zheng Liu <zheng.liu@grab.com>
2021-03-15 15:40:36 -0700

    Update test.js

--- assets/js/test.js
+++ assets/js/test.js
@@ -11,7 +11,7 @@ function loadjscssfile(filename){
 var bottomTag = document.createElement("div");
 bottomTag.style="height:20px;background-color:#DC222C;width:100%;text-align: center;";
 var text = document.createTextNode("Apply Your Credit Card Today.");
-// bottomTag.innerHTML = `<div id="smartbar-app" style="position: static;"><div class="smartbar-wrapper"><ul class="smartbar-barlist"><li id="smartbar-bar-133311" class="smartbar-bar smartbar-bar--bottom_cover smartbar-bar--active"><!----><div class="smartbar-bar__inner sb-flex-row sb-justify-content-center"><a href="#myModal" rel="modal:open">&nbsp;</a><div class="smartbar-object smartbar-object--message"><div class="smartbar-message" style="color: rgb(246, 243, 243);">Apply Imprint Credit Card today!</div></div></div><div class="smartbar-style" style="display: none;"><style type="text/css" id="smartbar-bar-133311-custom-style">
+// bottomTag.innerHTML = `<div id="smartbar-app" style="position: static;"><div class="smartbar-wrapper"><ul class="smartbar-barlist"><li id="smartbar-bar-133311" class="smartbar-bar smartbar-bar--bottom_cover smartbar-bar--active"><!----><div class="smartbar-bar__inner sb-flex-row sb-justify-content-center"><a href="#myModal" rel="modal:open">&nbsp;<div class="smartbar-object smartbar-object--message"><div class="smartbar-message" style="color: rgb(246, 243, 243);">Apply Imprint Credit Card today!</div></div></a></div><div class="smartbar-style" style="display: none;"><style type="text/css" id="smartbar-bar-133311-custom-style">
 // #smartbar-bar-133311 .smartbar-object,
 // #smartbar-bar-133311 .smartbar-message,
 // #smartbar-bar-133311 .smartbar-button,
@@ -37,7 +37,7 @@ document.body.appendChild(bottomTag);
 var topTag = document.createElement("div");
 //topTag.style="height:20px;background-color:#DC222C;width:100%;text-align: center;";
 //var text = document.createTextNode("Apply Your Credit Card Today.");
-topTag.innerHTML = `<div id="smartbar-app" style="position: static;"><div class="smartbar-wrapper"><ul class="smartbar-barlist"><li id="smartbar-bar-133084" class="smartbar-bar smartbar-bar--top_push smartbar-bar--active"><!----><div class="smartbar-bar__inner sb-flex-row sb-justify-content-center"><a href="#myModal" rel="modal:open">&nbsp;</a><div class="smartbar-object smartbar-object--message"><div class="smartbar-message" style="color: rgb(255, 255, 255);">Apply your Imprint credit card!</div></div></div><div class="smartbar-style" style="display: none;"><style type="text/css" id="smartbar-bar-133084-custom-style"> #smartbar-bar-133084 .smartbar-object, #smartbar-bar-133084 .smartbar-message, #smartbar-bar-133084 .smartbar-button, #smartbar-bar-133084 .smartbar-countdown .sbc-digit, #smartbar-bar-133084 .smartbar-countdown .sbc-unit { font-family: 'Overlock'; font-size: 16px; font-weight: 700; font-style: inherit; letter-spacing: 0px; } #smartbar-bar-133084 { background-color: #DC222C; background-image: ; background-size: auto; background-repeat: repeat; height: 45px; }</style></div></li></ul><div class="specify-theme-style" style="display: none;"><style type="text/css" id="smartbar-specific-theme-Debut"></style></div><!----></div></div>`;
+topTag.innerHTML = `<div id="smartbar-app" style="position: static;"><div class="smartbar-wrapper"><ul class="smartbar-barlist"><li id="smartbar-bar-133084" class="smartbar-bar smartbar-bar--top_push smartbar-bar--active"><!----><div class="smartbar-bar__inner sb-flex-row sb-justify-content-center"><a href="#myModal" rel="modal:open">&nbsp;<div class="smartbar-object smartbar-object--message"><div class="smartbar-message" style="color: rgb(255, 255, 255);">Apply your Imprint credit card!</div></div></a></div><div class="smartbar-style" style="display: none;"><style type="text/css" id="smartbar-bar-133084-custom-style"> #smartbar-bar-133084 .smartbar-object, #smartbar-bar-133084 .smartbar-message, #smartbar-bar-133084 .smartbar-button, #smartbar-bar-133084 .smartbar-countdown .sbc-digit, #smartbar-bar-133084 .smartbar-countdown .sbc-unit { font-family: 'Overlock'; font-size: 16px; font-weight: 700; font-style: inherit; letter-spacing: 0px; } #smartbar-bar-133084 { background-color: #DC222C; background-image: ; background-size: auto; background-repeat: repeat; height: 45px; }</style></div></li></ul><div class="specify-theme-style" style="display: none;"><style type="text/css" id="smartbar-specific-theme-Debut"></style></div><!----></div></div>`;
 document.body.appendChild(topTag);
 
 
@@ -49,7 +49,7 @@ var modal = document.createElement("div");
 modal.innerHTML = `<div id="myModal" class="modal">
 
   <!-- Modal content -->
-  <div class="modal-content">
+  <div class="modal-content" style="width:300px;height:600px;bacground-color:yellow">
     <span class="close">&times;</span>
     <p>Some text in the Modal..</p>
   </div>
