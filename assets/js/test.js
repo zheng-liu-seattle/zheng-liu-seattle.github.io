c7b5c2fd2c3da3a4d62da4b2ae72dc7360416bce
Zheng Liu <zheng.liu@grab.com>
2021-03-14 18:41:37 -0700

    Update test.js

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,26 +1,26 @@
 //bottom
 var bottomTag = document.createElement("div");
-//bottomTag.style="height:20px;background-color:#DC222C;width:100%;text-align: center;";
-//var text = document.createTextNode("Apply Your Credit Card Today.");
-bottomTag.innerHTML = `<div id="smartbar-app" style="position: static;"><div class="smartbar-wrapper"><ul class="smartbar-barlist"><li id="smartbar-bar-133311" class="smartbar-bar smartbar-bar--bottom_cover smartbar-bar--active"><!----><div class="smartbar-bar__inner sb-flex-row sb-justify-content-center"><a href="www.imprint.co" target="_blank" class="smartbar-bar__link">&nbsp;</a><div class="smartbar-object smartbar-object--message"><div class="smartbar-message" style="color: rgb(246, 243, 243);">Apply Imprint Credit Card today!</div></div></div><div class="smartbar-style" style="display: none;"><style type="text/css" id="smartbar-bar-133311-custom-style">
-#smartbar-bar-133311 .smartbar-object,
-#smartbar-bar-133311 .smartbar-message,
-#smartbar-bar-133311 .smartbar-button,
-#smartbar-bar-133311 .smartbar-countdown .sbc-digit,
-#smartbar-bar-133311 .smartbar-countdown .sbc-unit {
-  font-family: 'inherit';
-  font-size: 18px;
-  font-weight: 400;
-  font-style: inherit;
-  letter-spacing: 0px;
-}
-#smartbar-bar-133311 {
-  background-color: #1D1A1A;
-  background-image: ;
-  background-size: auto;
-  background-repeat: repeat;
-  height: 45px;
-}</style></div></li></ul><div class="specify-theme-style" style="display: none;"><style type="text/css" id="smartbar-specific-theme-Debut"></style></div><!----></div></div>`;
+bottomTag.style="height:20px;background-color:#DC222C;width:100%;text-align: center;";
+var text = document.createTextNode("Apply Your Credit Card Today.");
+// bottomTag.innerHTML = `<div id="smartbar-app" style="position: static;"><div class="smartbar-wrapper"><ul class="smartbar-barlist"><li id="smartbar-bar-133311" class="smartbar-bar smartbar-bar--bottom_cover smartbar-bar--active"><!----><div class="smartbar-bar__inner sb-flex-row sb-justify-content-center"><a href="www.imprint.co" target="_blank" class="smartbar-bar__link">&nbsp;</a><div class="smartbar-object smartbar-object--message"><div class="smartbar-message" style="color: rgb(246, 243, 243);">Apply Imprint Credit Card today!</div></div></div><div class="smartbar-style" style="display: none;"><style type="text/css" id="smartbar-bar-133311-custom-style">
+// #smartbar-bar-133311 .smartbar-object,
+// #smartbar-bar-133311 .smartbar-message,
+// #smartbar-bar-133311 .smartbar-button,
+// #smartbar-bar-133311 .smartbar-countdown .sbc-digit,
+// #smartbar-bar-133311 .smartbar-countdown .sbc-unit {
+//   font-family: 'inherit';
+//   font-size: 18px;
+//   font-weight: 400;
+//   font-style: inherit;
+//   letter-spacing: 0px;
+// }
+// #smartbar-bar-133311 {
+//   background-color: #1D1A1A;
+//   background-image: ;
+//   background-size: auto;
+//   background-repeat: repeat;
+//   height: 45px;
+// }</style></div></li></ul><div class="specify-theme-style" style="display: none;"><style type="text/css" id="smartbar-specific-theme-Debut"></style></div><!----></div></div>`;
 bottomTag.appendChild(text);
 document.body.appendChild(bottomTag);
 
@@ -29,5 +29,4 @@ var topTag = document.createElement("div");
 //topTag.style="height:20px;background-color:#DC222C;width:100%;text-align: center;";
 //var text = document.createTextNode("Apply Your Credit Card Today.");
 topTag.innerHTML = `<div id="smartbar-app" style="position: static;"><div class="smartbar-wrapper"><ul class="smartbar-barlist"><li id="smartbar-bar-133084" class="smartbar-bar smartbar-bar--top_push smartbar-bar--active"><!----><div class="smartbar-bar__inner sb-flex-row sb-justify-content-center"><a href="http://www.imprint.co" target="_blank" class="smartbar-bar__link">&nbsp;</a><div class="smartbar-object smartbar-object--message"><div class="smartbar-message" style="color: rgb(255, 255, 255);">Apply your Imprint credit card!</div></div></div><div class="smartbar-style" style="display: none;"><style type="text/css" id="smartbar-bar-133084-custom-style"> #smartbar-bar-133084 .smartbar-object, #smartbar-bar-133084 .smartbar-message, #smartbar-bar-133084 .smartbar-button, #smartbar-bar-133084 .smartbar-countdown .sbc-digit, #smartbar-bar-133084 .smartbar-countdown .sbc-unit { font-family: 'Overlock'; font-size: 16px; font-weight: 700; font-style: inherit; letter-spacing: 0px; } #smartbar-bar-133084 { background-color: #DC222C; background-image: ; background-size: auto; background-repeat: repeat; height: 45px; }</style></div></li></ul><div class="specify-theme-style" style="display: none;"><style type="text/css" id="smartbar-specific-theme-Debut"></style></div><!----></div></div>`;
-topTag.appendChild(text);
 document.body.appendChild(topTag);
